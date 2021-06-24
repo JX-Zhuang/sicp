67af957dbecee50aa05ee50b0ad6846ac8e09ad4
JX-Zhuang <who.zhuang@gmail.com>
2021-06-24 13:10:01 +0800

    update

--- 第1章 构造过程抽象/1.1 程序设计第基本元素.scm
+++ 第1章 构造过程抽象/1.1 程序设计第基本元素.scm
@@ -68,9 +68,11 @@
 ;     (sqrt-iter 1.0 x))
 
 
-
+(define (new-if predicate then-clause else-clause)
+    (cond (predicate then-cluase)
+        (else else-clause )))
 (define (sqrt-iter guess x)
-    (if(good-enough guess x)
+    (if (good-enough guess x)
         guess
     (sqrt-iter (improve guess x) x)))
 (define (improve guess x)
