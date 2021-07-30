f5ad8f396418be4d53a6bfb2277d5fa78118436b
JX-Zhuang <who.zhuang@gmail.com>
2021-07-30 13:54:25 +0800

    update

--- 第2章 构造数据抽象/2.2 层次性数据与闭包性质.scm
+++ 第2章 构造数据抽象/2.2 层次性数据与闭包性质.scm
@@ -61,3 +61,29 @@
 (same-parity 1 2 3 4 5 6 7)
 (same-parity 2 3 4 5 6 7)
 (same-parity 1)
+
+; 对表对映射
+(define (map proc items)
+    (if (null? items)
+        items
+        (cons (proc (car items))
+            (map proc (cdr items)))))
+(map abs (list -10 2.5 -11.6 17))
+(map (lambda (x) (* x x)) (list 1 2 3 4))
+; 练习 2.21
+(define (square-list items)
+    (if (null? items)
+        ()
+        (cons (square (car items)) (square-list (cdr items)))))
+(define (square-list items)
+    (map square items))
+(square-list (list 1 2 3 4))
+; 练习 2.23
+(define (for-each proc items)
+    (if (not (null? items))
+        (begin 
+        (proc (car items))
+        (for-each proc (cdr items)))))
+(for-each (lambda (x) (newline) (display x)) (list 57 321 88))
+
+; 2.2.2 层次性结构
