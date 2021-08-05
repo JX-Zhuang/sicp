4d4ff42d11454ff689a9c3d46630e3189d5389ed
JX-Zhuang <who.zhuang@gmail.com>
2021-08-05 13:45:17 +0800

    update

--- 第2章 构造数据抽象/2.2 层次性数据与闭包性质.scm
+++ 第2章 构造数据抽象/2.2 层次性数据与闭包性质.scm
@@ -179,3 +179,11 @@
 (define (length sequence)
     (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
 (length (list 1 2 3))
+
+; 练习 2.34
+(define (horner-eval x coefficient-sequence)
+    (accumulate (lambda (this-coeff higher-terms) 
+                    (+ this-coeff (* x higher-terms) )) 
+                    0 
+                    coefficient-sequence))
+(horner-eval 2 (list 1 3 0 5 0 1))
