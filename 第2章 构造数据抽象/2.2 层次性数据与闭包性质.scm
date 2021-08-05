f3ef22924ab7f5a41b11f9f3fb0daa1c2a9655c2
JX-Zhuang <who.zhuang@gmail.com>
2021-08-05 13:37:22 +0800

    update

--- 第2章 构造数据抽象/2.2 层次性数据与闭包性质.scm
+++ 第2章 构造数据抽象/2.2 层次性数据与闭包性质.scm
@@ -159,3 +159,23 @@
 ; 2.2.3 序列作为一种约定的界面
 
 (map square (list 1 2 3 4 5))
+(define (accumulate op initial sequence)
+    (if (null? sequence)
+        initial
+        (op (car sequence)
+            (accumulate op initial (cdr sequence)))))
+(accumulate + 0 (list 1 2 3 4 5))
+
+; 练习 2.33
+(define (map p sequence)
+    (accumulate (lambda (x y) 
+                    (cons (p x) y)) 
+                () 
+    sequence))
+(map square (list 1 2 3))
+(define (append seq1 seq2)
+    (accumulate cons seq2 seq1))
+(append (list 1 2 3) (list 4 5 6))
+(define (length sequence)
+    (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
+(length (list 1 2 3))
