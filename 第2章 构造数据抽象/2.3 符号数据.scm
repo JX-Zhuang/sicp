a475124ae512a540ab3d54a57c6b84c8ab1d383e
JX-Zhuang <who.zhuang@gmail.com>
2021-08-10 16:51:29 +0800

    update

--- 第2章 构造数据抽象/2.3 符号数据.scm
+++ 第2章 构造数据抽象/2.3 符号数据.scm
@@ -1,1 +1,7 @@
 ; 2.3.1 引号
+(define (memq item x)
+    (cond ((null? x) false)
+          ((eq? item (car x)) x)
+          (else (memq item (cdr x)))))
+(memq `apple `(pear banana prune))
+(memq `apple `(x (apple sauce) y apple pear))
