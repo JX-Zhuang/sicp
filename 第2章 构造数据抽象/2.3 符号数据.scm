d6f6c932ed4977f1de9a9500d329a006a7c23803
JX-Zhuang <who.zhuang@gmail.com>
2021-08-10 18:42:36 +0800

    update

--- 第2章 构造数据抽象/2.3 符号数据.scm
+++ 第2章 构造数据抽象/2.3 符号数据.scm
@@ -5,3 +5,32 @@
           (else (memq item (cdr x)))))
 (memq `apple `(pear banana prune))
 (memq `apple `(x (apple sauce) y apple pear))
+; 练习 2.53
+(list `a `b `c)
+(memq `red `(red shoes blue))
+(car `(red shoes blue))
+
+; 练习 2.54
+(define (equal? x y)
+    (cond ((and (symbol? x) (symbol? y))
+        (symbol-eq? x y))
+        ((and (list? x) (list? y))
+        (list-equal? x y))
+        (else
+            (error "error--" x y))))
+(define (symbol-eq? x y)
+    (eq? x y))
+(define (list-equal? x y)
+    (cond ((and (null? x) (null? y))
+        #t)
+          ((or (null? x) (null? y))
+        #f)
+        ((equal? (car x) (car y))
+         (equal? (cdr y) (cdr y)))
+        (else 
+         #f)))
+(list? (list 1 2 3))
+(equal? 'symbol 'symbol)
+(equal? 'symbol 'another-symbol)
+(equal? (list 'a 'b 'c) (list 'a 'b 'c))
+(equal? (list 'a) (list 'a 'b 'c))
