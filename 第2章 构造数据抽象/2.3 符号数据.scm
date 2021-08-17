831f800db04ed5faf35135cb4d3094163d61293e
JX-Zhuang <who.zhuang@gmail.com>
2021-08-17 13:02:02 +0800

    update

--- 第2章 构造数据抽象/2.3 符号数据.scm
+++ 第2章 构造数据抽象/2.3 符号数据.scm
@@ -200,3 +200,36 @@
     (if (leaf? tree)
         (weight-leaf tree)
         (caddr tree)))
+    
+(define (decode bits tree)
+    (define (decode-1 bits current-branch)
+        (if (null? bits)
+            `()
+            (let ((next-branch
+                    (choose-branch (car bits) current-branch)))
+                (if (leaf? next-branch)
+                    (cons (symbol-leaf next-branch)
+                          (decode-1 (cdr bits) tree))
+                    (decode-1 (cdr bits) next-branch)))))
+        (decode-1 bits tree))
+(define (choose-branch bit branch)
+    (cond ((= bit 0) (left-branch branch))
+          ((= bit 1) (right-branch branch))
+          (else (error "error:" bit))))
+(define (adjoin-set x set)
+    (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+(define (make-leaf-set pairs)
+    (if (null? pairs)
+        `()
+        (let ((pair (car pairs)))
+            (adjoin-set (make-leaf (car pair)
+                                   (cadr pair))
+                        (make-leaf-set (cdr pairs))))))
+; 练习 2.67
+
+; 2.4 抽象数据的多重表示
+
+
