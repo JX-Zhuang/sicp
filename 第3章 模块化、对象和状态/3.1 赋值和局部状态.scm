101223ea63f67453ac62396263673e3c1501de98
JX-Zhuang <who.zhuang@gmail.com>
2021-08-20 13:44:14 +0800

    update

--- 第3章 模块化、对象和状态/3.1 赋值和局部状态.scm
+++ 第3章 模块化、对象和状态/3.1 赋值和局部状态.scm
@@ -1,1 +1,10 @@
 ; 3.1.1 局部状态变量
+
+; 练习 3.1
+(define (make-accumulator value)
+    (lambda (add-value)
+        (set! value (+ value add-value))
+        value))
+ (define A (make-accumulator 5))
+ (A 10)
+ (A 5)
