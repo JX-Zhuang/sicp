7e86820d59f4bebd7e83587113bb55ac1c141b4f
JX-Zhuang <who.zhuang@gmail.com>
2021-09-10 13:50:51 +0800

    update

---
+++ 第4章 元语言抽象/4.1 元循环求值器.scm
@@ -0,0 +1,62 @@
+; 4.1.1 求值器的内核
+(define (eval exp env)
+    (cond ((self-evaluating? exp) exp)
+          ((variable? exp) (lookup-variable-value exp env))
+          ((quoted? exp) (text-of-quotation exp))
+          ((assignment? exp) (eval-assignment exp env))
+          ((definition? exp) (eval-definition exp env))
+          ((if? exp) (eval-if exp env))
+          ((lambda? exp)
+           (make-procedure (lambda-parameters exp)
+                           (lambda-body exp)
+                           env))
+          ((begin? exp)
+           (eval-sequence (begin-actions exp) env))
+          ((cond? exp) (eval (cond->if exp) env))
+          ((application? exp)
+           (apply (eval (operator exp) env)
+                  (list-of-values (operands exp) env)))
+          (else 
+            (error "eval error:" exp))))
+
+(define (apply procedure arguments)
+    (cond ((primitive-procedure? procedure)
+           (apply-primitive-procedure procedure arguments))
+          ((compound-procedure? procedure)
+           (eval-sequence
+                (procedure-body procedure)
+                (extend-environment
+                    (procedure-parameters procedure)
+                    arguments
+                    (procedure-environment procedure))))
+            (else
+                (error "apply error:" procedure))))
+; 过程参数
+(define (list-of-values exps env)
+    (if (no-operands? exps)
+        `()
+        (cons (eval (first-operand exps) env)
+            (list-of-values (rest-operands exps) evn))))
+; 条件
+(define (eval-if exp env)
+    (if (true? (eval (if-predicate exp) env))
+        (eval (if-consequent exp) env)
+        (eval (if-alternative exp) env)))
+; 序列
+(define (eval-sequence exps env)
+    (cond ((last-exp? exps) (eval (first-exp exps) env))
+          (else (eval (first-exp exps) env)
+                (eval-sequence (rest-exps exps) env))))
+; 赋值和定义
+(define (eval-assignment exp env)
+    (set-variable-value! (assignment-variable exp)
+                         (eval (assignment-value exp) env)
+                         env)
+    `ok)
+(define (eval-definition exp env)
+    (define-variable! (definition-variable exp)
+                     (eval (definition-value exp) env)
+                     env)
+    `ok)
+
+; 4.1.2 表达式的表示
