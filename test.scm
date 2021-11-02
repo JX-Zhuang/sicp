f52d06b2eedec9248188bf67dd6a2e6d25c6e828
PlayerWho <who.zhuang@gmail.com>
2021-11-02 17:10:10 +0800

    Update test.scm

--- test.scm
+++ test.scm
@@ -3,16 +3,8 @@
 (define pi 3.14159)
 (define radius 10)
 (* pi (* radius radius))
-
 (define (square x)
     (* x x))
 (define (sum-of-squares x y)
     (+ (square x) (square y)))
 (sum-of-squares 3 4)
-
-
-
-
-
-
-
